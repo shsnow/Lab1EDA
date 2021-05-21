dd11c3775084c1f68446556f6591133857c8f724
shsnow <jonasoviedo574@gmail.com>
2021-05-21 16:21:57 -0400

    lab 2 keeeee

--- laboratorio2/Laboratorio2.java
+++ laboratorio2/Laboratorio2.java
@@ -1,11 +1,9 @@
-package laboratorio2;
 
 
-import Laboratorio2.MaxPQ;
-import Laboratorio2.tripleta;
 import java.util.*; 
 import java.io.*;   
-import java.lang.*; 
+import java.lang.*;
+import java.lang.reflect.Array; 
 
 public class Laboratorio2{
     public static void main(String[] args) throws IOException{
@@ -15,8 +13,8 @@ public class Laboratorio2{
       
         
         //File folder = new File("./csv");
-        File folder = new File("C:\\Users\\Cristián Rodríguez\\Documents\\NetBeansProjects\\Laboratorio2\\src\\laboratorio2\\csv");
-        //File folder = new File("C:\\Users\\Jonas\\Documents\\GitHub\\Lab1EDA\\csv");
+        //File folder = new File("C:\\Users\\Cristián Rodríguez\\Documents\\NetBeansProjects\\Laboratorio2\\src\\laboratorio2\\csv");
+        File folder = new File("C:\\Users\\Jonas\\Documents\\GitHub\\Lab1EDA\\laboratorio2\\csv");
         
         File[] fileList = folder.listFiles();
         ArrayList<String> files = new ArrayList<String>();//nombres
@@ -29,7 +27,8 @@ public class Laboratorio2{
         */
         MaxPQ<tripleta> Bigpq = new MaxPQ<tripleta>();
         ArrayList<tripleta> BigArr = new ArrayList<tripleta>();
-        
+        ArrayList<ArrayList<tripleta>> dataSet_Triple = new ArrayList<ArrayList<tripleta>>();
+        ArrayList<tripleta> triple_data = new ArrayList<tripleta>();
         int cont =0;
         for(int i = 0; i < fileList.length; i++){
             if(fileList[i].isFile()){
@@ -41,7 +40,8 @@ public class Laboratorio2{
                     e.printStackTrace();
                 }
             } 
-        }try{
+        }
+        try{
             
             int largo = filenames.get(0).length();  
             String outfilename = filenames.get(0).substring(0, largo-4) + new String("_tres") + filenames.get(0).substring(largo-4);
@@ -87,136 +87,140 @@ public class Laboratorio2{
                     }     
                 }
                 //datasets.add(dataset);
-                //datasets_triple.add(t_count); 
+                //añade trpleta a arraylist de tripletas
+                for(tripleta  t: t_count){
+                    triple_data.add(t);
+                } 
            
-           //Lista usada para los ordenamientos
-            ArrayList<tripleta> datasetSort = new ArrayList<tripleta>(); 
-            BigArr.addAll(t_count);
-            datasetSort.addAll(t_count);
+                //Lista usada para los ordenamientos
+                ArrayList<tripleta> datasetSort = new ArrayList<tripleta>(); 
+                BigArr.addAll(t_count);
+                datasetSort.addAll(t_count);
 
-            /*System.out.println("Arreglo desordenado" + cont );
-            for(tripleta t : datasetSort){
-                System.out.print(t.get_conteo() + " ");
-            }
-            System.out.println("");
-      */
-            //necesitamos la Cola de prioridad en todos los ciclos
-            MaxPQ<tripleta> pq = new MaxPQ<tripleta>();
-            
-        if(cont == 0){//para que funcione en el primer archivo.
- 
-            SelectionSort selectionSort = new SelectionSort();
+                /*System.out.println("Arreglo desordenado" + cont );
+                for(tripleta t : datasetSort){
+                    System.out.print(t.get_conteo() + " ");
+                }
+                System.out.println("");
+                */
+                //necesitamos la Cola de prioridad en todos los ciclos
+                MaxPQ<tripleta> pq = new MaxPQ<tripleta>();
                 
-            //Selection Sort:
-            long nano_startTime1 = System.nanoTime();
-            selectionSort.sort(datasetSort);
-            long nano_endTime1 = System.nanoTime();
+                if(cont == 0){//para que funcione en el primer archivo.
+        
+                    SelectionSort selectionSort = new SelectionSort();
+                        
+                    //Selection Sort:
+                    long nano_startTime1 = System.nanoTime();
+                    selectionSort.sort(datasetSort);
+                    long nano_endTime1 = System.nanoTime();
+                    
+                    /*
+                    System.out.println("Arreglo ordenado con SelectionSort:");
+                    selectionSort.printArray(datasetSort);
+                    */
+                    
+                    System.out.println("");
+                    
+                    System.out.println("Tiempo ejecución SelectionSort nano segundos: " + (nano_endTime1 - nano_startTime1 ));
+                    
+                    Collections.copy(datasetSort,t_count);
+                    
+                    /* System.out.println("Arreglo desordenado");
+                    for(tripleta t : datasetSort){
+                        System.out.print(t.get_conteo() + " ");
+                    }*/
+                    System.out.println("");
+                    
+                    //MERGE SORT:
+                    MergeSort mergeSort = new MergeSort(datasetSort);
+                
+                    long nano_startTime2 = System.nanoTime();
+                    mergeSort.sort(0, datasetSort.size()-1);
+                    long nano_endTime2 = System.nanoTime();
+                    System.out.println("Tiempo ejecución MergeSort nano segundos: " + (nano_endTime2 - nano_startTime2 ));
+                    
+                    /*System.out.println("Arreglo ordenado con MergeSort:");
+                    for(tripleta t : datasetSort){
+                        System.out.print(t.get_conteo() + " ");
+                    }*/
+                    System.out.println("");
             
-            /*
-            System.out.println("Arreglo ordenado con SelectionSort:");
-            selectionSort.printArray(datasetSort);
-            */
-            
-            System.out.println("");
-            
-            System.out.println("Tiempo ejecución SelectionSort nano segundos: " + (nano_endTime1 - nano_startTime1 ));
-            
-            Collections.copy(datasetSort,t_count);
-            
-           /* System.out.println("Arreglo desordenado");
-            for(tripleta t : datasetSort){
-                System.out.print(t.get_conteo() + " ");
-            }*/
-            System.out.println("");
-            
-           //MERGE SORT:
-           MergeSort mergeSort = new MergeSort(datasetSort);
-           
-           long nano_startTime2 = System.nanoTime();
-            mergeSort.sort(0, datasetSort.size()-1);
-             long nano_endTime2 = System.nanoTime();
-            System.out.println("Tiempo ejecución MergeSort nano segundos: " + (nano_endTime2 - nano_startTime2 ));
-              
-            /*System.out.println("Arreglo ordenado con MergeSort:");
-            for(tripleta t : datasetSort){
-                System.out.print(t.get_conteo() + " ");
-            }*/
-            System.out.println("");
-       
-   
-            Collections.copy(datasetSort,t_count);
-            
-            /*System.out.println("Arreglo desordenado");
-            for(tripleta t : datasetSort){
-                System.out.print(t.get_conteo() + " ");
-            }*/
-            System.out.println("");
-            
+        
+                    Collections.copy(datasetSort,t_count);
+                    
+                    /*System.out.println("Arreglo desordenado");
+                    for(tripleta t : datasetSort){
+                        System.out.print(t.get_conteo() + " ");
+                    }*/
+                    System.out.println("");
+                    
 
-           //HEAP SORT:
-            //System.out.println("Arreglo ordenado con HeapSort");
-            tripleta[] data;
-            data = new tripleta[datasetSort.size()];
+                    //HEAP SORT:
+                    //System.out.println("Arreglo ordenado con HeapSort");
+                    tripleta[] data;
+                    data = new tripleta[datasetSort.size()];
+                    
+                    long nano_startTime3 = System.nanoTime();
+                    for(int i =0; i<datasetSort.size(); i++){
+                        data[i] = datasetSort.get(i);    
+                        pq.insert(data[i]);
+                        Bigpq.insert(data[i]);
+                    }
+                    long nano_endTime3 = System.nanoTime();
+                    System.out.println("Tiempo ejecución HeapSort nano segundos: " + (nano_endTime3 - nano_startTime3 ));
+                            
+                    /* while(!pq.isEmpty()){
+                        System.out.print(pq.delMax().get_conteo() + " ");
+                    }
+                        */
+                }
+                else{//otros ciclos
+                    tripleta[] data;
+                    data = new tripleta[datasetSort.size()];
+                
+                    for(int i =0; i<datasetSort.size(); i++){
+                        data[i] = datasetSort.get(i);    
+                        pq.insert(data[i]);
+                        Bigpq.insert(data[i]);
+                    }
             
-            long nano_startTime3 = System.nanoTime();
-            for(int i =0; i<datasetSort.size(); i++){
-            data[i] = datasetSort.get(i);    
-            pq.insert(data[i]);
-            Bigpq.insert(data[i]);
-            }
-            long nano_endTime3 = System.nanoTime();
-            System.out.println("Tiempo ejecución HeapSort nano segundos: " + (nano_endTime3 - nano_startTime3 ));
-            
-           /* while(!pq.isEmpty()){
-            System.out.print(pq.delMax().get_conteo() + " ");
-        }
-            */
-            }else{//otros ciclos
-            tripleta[] data;
-            data = new tripleta[datasetSort.size()];
-            
-            for(int i =0; i<datasetSort.size(); i++){
-            data[i] = datasetSort.get(i);    
-            pq.insert(data[i]);
-            Bigpq.insert(data[i]);
-            }
-            
-        }
+                }
                 
                
-        //PQ que obtiene los 3 elementos más y menos comprados de cada archivo csv.
-         tripleta[] data;
-            data = new tripleta[t_count.size()];
+                //PQ que obtiene los 3 elementos más y menos comprados de cada archivo csv.
+                tripleta[] data;
+                data = new tripleta[t_count.size()];
             
-            for(int i =0; i<t_count.size(); i++){
-            data[i] = t_count.get(i);    
-            pq.insert(data[i]);
-            }    
+                for(int i =0; i<t_count.size(); i++){
+                    data[i] = t_count.get(i);    
+                    pq.insert(data[i]);
+                }    
             
-        ArrayList<String> tres = new ArrayList<String>();
-        tres =  pq.print3(pq);
-        //System.out.println(tres);
+                ArrayList<String> tres = new ArrayList<String>();
+                tres =  pq.print3(pq);
+                //System.out.println(tres);
            
-        writer.write("Tres productos más y menos solicitados de " +cat + ":" + "\n");
-        for(int i =0; i< tres.size(); i++){
-            writer.write(tres.get(i) + "\n");
-            }
-            writer.write("\n");
-            
-         //BST que responde a preg del usuario.
+                writer.write("Tres productos más y menos solicitados de " +cat + ":" + "\n");
+                for(int i =0; i< tres.size(); i++){
+                    writer.write(tres.get(i) + "\n");
+                }
+                writer.write("\n");
+                    
+                //BST que responde a preg del usuario.
            
             
             
             
-            //Ideas: meter TODAS tripletas en la pq, de forma que esten ordenadas,
+                //Ideas: meter TODAS tripletas en la pq, de forma que esten ordenadas,
                     //luego podemos meterlas con su contador asociado a la cantidad comprada
                     //ahi hacer el cambio en el bst tal que si 2 tienen mismo contador, las meta en el arraylist de tripletas
             
             
             
             
-        reader.close();
-        cont++;  
+                reader.close();
+                cont++;  
             }
              writer.close();
              System.out.println(Bigpq.size());
@@ -248,7 +252,36 @@ public class Laboratorio2{
                 primero = true;
             }
             }
-            
+            //la 4
+            BST<Integer, ArrayList<tripleta>> theArbol = new BST<Integer,ArrayList<tripleta>>();
+            //<cantidad, tripletas con esa cantidad>
+            //definir tripleta con los conteo
+            //añadir tripletas de a una
+            Map<Integer,ArrayList<tripleta>> cantidadTriple = new HashMap<Integer,ArrayList<tripleta>>();
+            ArrayList<tripleta> auxi;
+            for(tripleta tri : triple_data){
+
+                if(cantidadTriple.containsKey(tri.get_conteo())){
+                    cantidadTriple.get(tri).add(tri);
+                    
+                }
+                else{
+                    auxi = new ArrayList<tripleta>();
+                    auxi.add(tri);
+                    cantidadTriple.put(tri.get_conteo(),auxi);
+                }
+
+            }   
+            //meter todo al theArbol
+            for(Integer k: cantidadTriple.keySet()){
+                theArbol.put(k, cantidadTriple.get(k));
+            }  
+
+            //crear arraylist para cada cantidad de ventaas meter todos con la misma cantidad a ese arreglo
+
+
+            //fin 4
+            //test
             Scanner sc = new Scanner(System.in);
             System.out.println("Qué desea hacer? Seleccione un numero: ");
             System.out.println("1. Productos que han vendido más de x unidades");
@@ -266,21 +299,21 @@ public class Laboratorio2{
             }
             
             if(opcion ==2){
-             System.out.println("Ingrese unidad x");
+                System.out.println("Ingrese unidad x");
                 int x = sc.nextInt();
                 
                 System.out.println(bst.keys(bst.min(),x));
                 
             }
             if(opcion == 3){
-             System.out.println("Ingrese unidad x");
+                System.out.println("Ingrese unidad x");
                 int x = sc.nextInt();
                 
                 bst.get(x).forEach((t) -> {
                     System.out.println("producto " + t.get_producto() + "conteo "+ t.get_conteo());
                 });
             }
-            
+            // fin test
             
         }
         catch(NullPointerException e){
